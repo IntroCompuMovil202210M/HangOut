743844b3fd01c1ac580b202c392e67d214411af4
estefania_bermudeza <estefanibearroyo@gmail.com>
2022-03-09 01:12:51 -0500

    Modify views

--- app/src/main/java/com/example/proyecto1/ModifyActivity.java
+++ app/src/main/java/com/example/proyecto1/ModifyActivity.java
@@ -2,13 +2,52 @@ package com.example.proyecto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ModifyActivity extends AppCompatActivity {
+
+    ImageView fav, location, logOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify);
+
+        //Inflate
+        fav = findViewById(R.id.favBtn);
+        location = findViewById(R.id.locationBtn);
+        logOut = findViewById(R.id.logOut);
+
+        //Action when fav button is clicked
+        fav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ModifyActivity.this, FavoriteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Action when location button is clicked
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ModifyActivity.this, AddressActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Action when logOut button is clicked
+        logOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ModifyActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
