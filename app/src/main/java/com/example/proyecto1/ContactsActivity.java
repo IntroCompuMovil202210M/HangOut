22ecfce0ebe77160f1aceac1851d0855e99a0b97
estefania_bermudeza <estefanibearroyo@gmail.com>
2022-05-02 10:29:48 -0500

    List of contacts created

--- app/src/main/java/com/example/proyecto1/ContactsActivity.java
+++ app/src/main/java/com/example/proyecto1/ContactsActivity.java
@@ -1,28 +1,75 @@
 package com.example.proyecto1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ContactsActivity extends AppCompatActivity {
-    ImageButton chat;
+
+    public static final String CONTACTS_PERMISSION_NAME = Manifest.permission.READ_CONTACTS;
+    public static final int CONTACTS_PERMISSION_ID = 5;
+
+    String [] mProjection;
+    Cursor mCursor;
+    ContactsAdapter mContactsAdapter;
+    ListView mlista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.item_lista_contactos);
-        chat = findViewById(R.id.chat_btn);
+        setContentView(R.layout.activity_contacts);
 
-        chat.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Pasa a la pantalla principal.
-                Intent intent= new Intent(getBaseContext(), ChatActivity.class);
-                startActivity(intent);
+        mlista = findViewById(R.id.list_contactos);
+        mProjection = new String[]{
+                ContactsContract.Profile._ID,
+                ContactsContract.Profile.DISPLAY_NAME_PRIMARY,
+        };
+
+        mContactsAdapter = new ContactsAdapter(this, null, 0);
+        mlista.setAdapter(mContactsAdapter);
+        requestPermission(this, CONTACTS_PERMISSION_NAME, "¿Permite tener acceso a sus contactos?", CONTACTS_PERMISSION_ID);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initView();
+    }
+
+    private void initView(){
+        if(ContextCompat.checkSelfPermission(this, CONTACTS_PERMISSION_NAME) == PackageManager.PERMISSION_GRANTED){
+            mCursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, mProjection, null, null, null);
+            mContactsAdapter.changeCursor(mCursor);
+        }
+    }
+
+    private void requestPermission(Activity context, String permission, String justification, int id){
+        if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context, permission)){
+                Toast.makeText(context, justification, Toast.LENGTH_LONG).show();
             }
-        });
+            ActivityCompat.requestPermissions(context, new String[]{permission}, id);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == CONTACTS_PERMISSION_ID){
+            initView();
+        }
     }
 }
